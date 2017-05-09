2f4ee15309e6d356e804b01be51d70e987532deb
Ruben ten Hove <git@rhtenhove.nl>
2017-05-09 10:13:34 +0200

    More ways to run the Spyder container

--- python/spyder/README.md
+++ python/spyder/README.md
@@ -1,1 +1,19 @@
 # Spyder
+
+### Using an alias
+
+One way to run this is to add an alias to your .bashrc.
+
+First build the container:
+
+`docker build ./spyder -t python-spyder`
+
+Then add the following line to `.bashrc`:
+
+alias spyderd="xhost +local:spyder && docker run -it -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix python-spyder && xhost -local:spyder"
+
+Don't forget to `source .bashrc`.
+
+### Running directly
+
+Run `startcontainer.sh`, ie.: `./startcontainer.sh`.
--- python/spyder/startcontainer.sh
+++ python/spyder/startcontainer.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-# This script gives the container access to the host xserver, and then (builds and) runs the container.
+# First move the working directory to the directory of this script, so this can be run from anywhere.
+cd "$(dirname "$0")"
 
+# Then give any machine with the hostname spyder access to the X11 socket.
 xhost +local:spyder
+
+# Start the container and attach to it.
 docker-compose up
+
+# Revoke access to the X11 socket when the script stops (ie. spyder is closed and the container ends).
+xhost -local:spyder
