e039c592e57badee2509fb93f2b35efd09ea6c2c
Ruben ten Hove <git@rhtenhove.nl>
2017-05-09 22:15:09 +0200

    phpmyadmin container

---
+++ phpmyadmin/config/config.user.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+ $cfg['Servers'][$i]['verbose'] = 'mariadb';
+ $cfg['Servers'][$i]['host'] = 'mariadb';
+ $cfg['Servers'][$i]['port'] = '3306';
+ $cfg['Servers'][$i]['socket'] = '';
+ $cfg['Servers'][$i]['connect_type'] = 'tcp';
+ $cfg['Servers'][$i]['extension'] = 'mysqli';
+ $cfg['Servers'][$i]['auth_type'] = 'cookie';
+ $cfg['Servers'][$i]['AllowNoPassword'] = false;
